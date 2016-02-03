ec993432f4512c458424c076b98ab203f7e77e3e
marcojakob <majakob@gmx.ch>
2016-02-03 01:14:56 +0100

    Support private GitHub repositories through access token in description

--- main.py
+++ main.py
@@ -2,28 +2,50 @@ from __future__ import print_function
 
 import json
 import logging
+import boto3
+import os
 
-import urllib
+import urllib2
 import zipfile
 import subprocess
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+lambda_client = boto3.client('lambda')
+
 def lambda_handler(event, context):
+    # Load the description from the Lambda Function Console. This is
+    # a workaround to get some sort of runtime configuration.
+    # We use it for the GitHub access token.
+    github_token = lambda_client.get_function_configuration(FunctionName=context.function_name)['Description']
+  
     #logger.info("Event: " + str(event))
     message = json.loads(event['Records'][0]['Sns']['Message'])
     #logger.info("Message: " + str(message))
 
     repourl = message['repository']['url']
     reponame = message['repository']['name']
+    repofullname = message['repository']['full_name']
+    
+    downloadUrl = "https://api.github.com/repos/" + repofullname + "/zipball";
 
-    logger.info("This is the URL: " + str(repourl))
+    logger.info("Downloading from: " + downloadUrl)
 
-    urllib.urlretrieve (repourl + "/archive/master.zip", "/tmp/master.zip")
-    zfile = zipfile.ZipFile('/tmp/master.zip')
+    # Download zip from GitHub with authentication headers. 
+    req = urllib2.Request(downloadUrl, headers = { 'Authorization' : 'token ' + github_token })
+    
+    with open("/tmp/repo.zip", "wb") as f:
+        f.write(urllib2.urlopen(req).read())
+        
+    #urllib.urlretrieve (repourl + "/archive/master.zip", "/tmp/repo.zip")
+    zfile = zipfile.ZipFile('/tmp/repo.zip')
     zfile.extractall("/tmp")
-    builddir = "/tmp/" + reponame + "-master/"
+    os.remove('/tmp/repo.zip')
+    
+    # Get the folder name of the extracted repository.
+    builddir = '/tmp/' + os.listdir('/tmp')[0] + "/"
+    #builddir = "/tmp/" + reponame + "-master/"
 
     subprocess.call("/var/task/hugo_0.15_linux_amd64.go" , shell=True, cwd=builddir)
     
